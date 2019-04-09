371b1de449aa518091e0515814644ed854755309
Alison Williams <alison.lauriewill@gmail.com>
2019-04-09 16:57:40 +0100

    First fish test passed

---
+++ specs/fish_spec.rb
@@ -0,0 +1,15 @@
+require("minitest/autorun")
+require("minitest/rg")
+require("./fish.rb")
+
+class FishTest < MiniTest::Test
+
+  def setup
+    @fish = Fish.new("Clive")
+  end
+
+  def test_return_fish
+    assert_equal("Clive", @fish.name  )
+  end
+
+end
